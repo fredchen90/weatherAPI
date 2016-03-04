cbf1e51d4a19db25190016baced12e3667c70385
fredchen90 <fredchen90@gmail.com>
2016-03-04 16:19:34 +0800

    重構JS Code
    1.將html string改成可讀性更高

--- weatherAPI/myscripts_r2.js
+++ weatherAPI/myscripts_r2.js
@@ -4,9 +4,11 @@ function genHtml(json,listIds,dateId,weatherId1,weatherId2,desciption,speed,clou
 	var getDate_1 = new Date((json_obj.list[listIds].dt)*1000).getDate();
 	var getMonth_1 = new Date((json_obj.list[listIds].dt)*1000).getMonth();
 
-	// var tempHtml = "<div><hr><div id='"+dateId+"'class='date i04d'></div><div class='desciption'><span id='"+weatherId1+"'class='label label-warning icon-box'>21.26</span><span id='"+weatherId2+"' class='label label-default icon-box'>21.98</span><i id='"+desciption+"'>broken clouds</i><p id='"+detailInfo+"'>7.07m/s<br>clouds: 56%, 1037.29 hpa</p></div></div>";
 	var weather_icon = "i" + json_obj.list[listIds].weather[0].icon;
-	var tempHtml = "<div><hr><div  class='date-weather-block '><span id='"+dateId+"' class='date'></span><span class='icon "+weather_icon+"'></span></div><div class='desciption'><span id='"+weatherId1+"'class='label label-warning icon-box'>21.26</span><span id='"+weatherId2+"' class='label label-default icon-box'>21.98</span><i id='"+desciption+"'>broken clouds</i><p><span id='"+speed+"' style='display:block;'></span><span id='"+clouds+"' style='display:inline-block;'></span><span id='"+pressure+"' style='display:inline-block;'></span></p></div></div>";
+	var tempHtml = "<div><hr><div  class='date-weather-block '><span id='"+dateId+"' class='date'></span><span class='icon "+weather_icon+"'></span>"+
+				"</div><div class='desciption'><span id='"+weatherId1+"'class='label label-warning icon-box'>21.26</span><span id='"+weatherId2+"' class='label label-default icon-box'>"+
+				"</span><i id='"+desciption+"'>broken clouds</i><p><span id='"+speed+"' style='display:block;'></span><span id='"+clouds+"' style='display:inline-block;'></span>"+
+				"<span id='"+pressure+"' style='display:inline-block;'></span></p></div></div>";
 
 	document.getElementById("myDIV").innerHTML += tempHtml;
 
@@ -18,53 +20,6 @@ function genHtml(json,listIds,dateId,weatherId1,weatherId2,desciption,speed,clou
 	document.getElementById(speed).innerHTML = json_obj.list[listIds].speed + "m/s";
 	document.getElementById(clouds).innerHTML = "clouds: " + json_obj.list[listIds].clouds + "%, ";
 	document.getElementById(pressure).innerHTML = json_obj.list[listIds].pressure + " hpa";
-
-// 	var hr = document.createElement("hr");
-// 	var div1 = document.createElement("div");
-// 	var div2 = document.createElement("div");
-// 	var div3 = document.createElement("div");
-// 	var span1 = document.createElement("span");
-// 	var span2 = document.createElement("span");
-// 	var i = document.createElement("i");、
-// 	var p = document.createElement("p");
-
-// // id
-// 	div2.id = dateId;
-// 	span1.id = weatherId1;
-// 	span2.id = weatherId2;
-// 	i.id = desciption;
-// 	p.id = detailInfo;
-// // class
-// 	div2.className  = "date";
-// 	div3.className = "desciption";
-// 	// span1.className = "label label-warning icon-box";
-// 	// span2.className = "label label-default icon-box";
-// // append
-// 	div3.appendChild(span1);
-// 	div3.appendChild(span2);
-// 	div3.appendChild(i);
-// 	div3.appendChild(p);
-// 	div1.appendChild(div2);
-// 	div1.appendChild(div3);
-// // append to top div
-// 	document.getElementById("myDIV").appendChild(hr);
-// 	document.getElementById("myDIV").appendChild(div1);
-
-// 	// add class
-// 	document.getElementById(dateId).classList.add("i" + json_obj.list[listIds].weather[0].icon);
-// 	document.getElementById(weatherId1).classList.add("label");
-// 	document.getElementById(weatherId1).classList.add("label-warning");
-// 	document.getElementById(weatherId1).classList.add("icon-box");
-// 	document.getElementById(weatherId2).classList.add("label");
-// 	document.getElementById(weatherId2).classList.add("label-default");
-// 	document.getElementById(weatherId2).classList.add("icon-box");
-
-// 	// fill value
-// 	document.getElementById(dateId).innerHTML = getDate_1 + " " + month[getMonth_1];
-// 	document.getElementById(weatherId1).innerHTML = json_obj.list[listIds].temp.min;
-// 	document.getElementById(weatherId2).innerHTML = json_obj.list[listIds].temp.max;
-// 	document.getElementById(desciption).innerHTML = json_obj.list[listIds].weather[0].description;
-// 	document.getElementById(detailInfo).innerHTML = json_obj.list[listIds].speed + "m/s" + "<br>" + "clouds: " + json_obj.list[listIds].clouds + "%, " + json_obj.list[listIds].pressure + " hpa";
 }
 
 
