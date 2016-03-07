b46b38d784cf16f7daa2102f63db35b5319af312
fredchen90 <fredchen90@gmail.com>
2016-03-07 17:51:33 +0800

    功能: Tabs + Dropdown list
    1.做出一個基本樣板

--- weatherAPI/myscripts_r2.js
+++ weatherAPI/myscripts_r2.js
@@ -1,4 +1,27 @@
-function genHtml(json,listIds,dateId,weatherId1,weatherId2,desciption,speed,clouds,pressure){
+function currentCity(){
+	var tab_currentCity = document.getElementById("tab_currentCity");
+	var tab_nextDay = document.getElementById("tab_nextDay");
+	tab_currentCity.className = "tab-focus";
+	tab_nextDay.className = "tab";
+	document.getElementById("currentCity").style.display = "block";
+	document.getElementById("nextDay").style.display = "none";
+	document.getElementById("dropdown_city").style.display = "block";
+	document.getElementById("dropdown_days").style.display = "none";
+}
+
+function nextDay(){
+	var tab_nextDay = document.getElementById("tab_nextDay");
+	var tab_currentCity = document.getElementById("tab_currentCity");
+	tab_nextDay.className = "tab-focus";
+	tab_currentCity.className = "tab";
+	document.getElementById("nextDay").style.display = "block";
+	document.getElementById("currentCity").style.display = "none";
+	document.getElementById("dropdown_days").style.display = "block";
+	document.getElementById("dropdown_city").style.display = "none";
+
+}
+
+function genNextDayHtml(json,listIds,dateId,weatherId1,weatherId2,desciption,speed,clouds,pressure){
 	var json_obj = json;
 	var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	var getDate_1 = new Date((json_obj.list[listIds].dt)*1000).getDate();
@@ -24,6 +47,24 @@ function genHtml(json,listIds,dateId,weatherId1,weatherId2,desciption,speed,clou
 	document.getElementById(pressure).innerHTML = json_obj.list[listIds].pressure + " hpa";
 }
 
+function genCurrentCityHtml(json,listIds,city_name){
+	var json_obj = json;
+	var get_time = new Date();
+	weather_icon = "i" + json_obj.list[listIds].weather[0].icon;
+
+	document.getElementById("get_time").innerHTML = "get at " + get_time.getFullYear() + "." + (get_time.getMonth()+1)  + "." + get_time.getDate() + " " + get_time.getHours() + ":" +  get_time.getMinutes(); 
+	document.getElementById("city_name").innerHTML = city_name;
+	document.getElementById("city_weather-img").classList.add(weather_icon);
+	document.getElementById("city_temp").innerHTML = json_obj.list[listIds].temp.day;
+	document.getElementById("city_desciption").innerHTML = json_obj.list[listIds].weather[0].description;
+	document.getElementById("city_cloud").innerHTML = json_obj.list[listIds].weather[0].description;
+	document.getElementById("city_wind").innerHTML = json_obj.list[listIds].speed + "m/s";
+	document.getElementById("city_pressure").innerHTML = json_obj.list[listIds].pressure + " hpa";
+	document.getElementById("city_humidity").innerHTML = json_obj.list[listIds].humidity;
+	document.getElementById("city_sunrise").innerHTML = "Unknow";
+	document.getElementById("city_sunset").innerHTML = "Unknow";
+	document.getElementById("city_geocoords").innerHTML = "Unknow";
+}
 
 function getJson(url,method,callback) {
 	var httpXmlRequest = new XMLHttpRequest();
@@ -48,9 +89,11 @@ var pressure = ["pressure_1", "pressure_2", "pressure_3", "pressure_4", "pressur
 
 function mainFunc(){
 	var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Taipei&mode=json&units=metric&cnt= 7&appid=44db6a862fba0b067b1930da0d769e98";
+	var city_name = "Taipei, TW";
 	getJson(url,"GET",function(json_obj){
+		genCurrentCityHtml(json_obj,0,city_name);
 		for (var i=0; i < date.length; i++){
-			genHtml(json_obj,i,date[i],weather1[i],weather2[i],desciption[i],speed[i],clouds[i],pressure[i]);
+			genNextDayHtml(json_obj,i,date[i],weather1[i],weather2[i],desciption[i],speed[i],clouds[i],pressure[i]);
 		}
 	});
 }
