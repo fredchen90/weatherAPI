5badd3cdd836bd87078a546d5fde5309803c43ee
fredchen90 <fredchen90@gmail.com>
2016-03-11 10:40:25 +0800

    修改部分code為JQuery-part2

--- weatherAPI/jquery_version/myscripts.js
+++ weatherAPI/jquery_version/myscripts.js
@@ -11,19 +11,6 @@ function onChangeForDays(days){
 }
 
 function onChangeForCity(city){
-	city_name = "Taipei, TW";
-	DomId_get_time = document.getElementById("get_time");
-	DomId_city_name = document.getElementById("city_name");
-	DomId_city_weather_img = document.getElementById("city_weather-img");
-	DomId_city_temp = document.getElementById("city_temp");
-	DomId_city_desciption = document.getElementById("city_desciption");
-	DomId_city_cloud = document.getElementById("city_cloud");
-	DomId_city_wind= document.getElementById("city_wind");
-	DomId_city_pressure = document.getElementById("city_pressure");
-	DomId_city_humidity = document.getElementById("city_humidity");
-	DomId_city_sunrise = document.getElementById("city_sunrise");
-	DomId_city_sunset = document.getElementById("city_sunset");
-	DomId_city_geocoords = document.getElementById("city_geocoords");
 	switch (city){
 		case 'taipei':
 		default:
@@ -43,8 +30,7 @@ function onChangeForCity(city){
 			break;
 	}
 	xHR(url,"GET","xml",function(xml_obj){
-		genCurrentCityHtml(xml_obj,0,id,DomId_get_time,DomId_city_name,DomId_city_weather_img,DomId_city_temp,DomId_city_desciption,DomId_city_cloud,DomId_city_wind,
-			DomId_city_pressure,DomId_city_humidity,DomId_city_sunrise,DomId_city_sunset,DomId_city_geocoords);
+		genCurrentCityHtml(xml_obj,0,id);
 	});
 }
 
@@ -52,6 +38,7 @@ function currentCity(){
 	var tab_currentCity = document.getElementById("tab_currentCity");
 	var tab_nextDay = document.getElementById("tab_nextDay");
 	tab_currentCity.className = "tab-focus";
+	tab_nextDay.className = "tab";
 	$("#container_currentCity").show();
 	$("#container_nextday").hide();
 }
@@ -82,30 +69,28 @@ function genNextDayHtml(json,month,listIds,DomId_nextday){
 	DomId_nextday.innerHTML += tempHtml;
 }
 
-function genCurrentCityHtml(xml,listIds,city_id,DomId_get_time,DomId_city_name,DomId_city_weather_img,DomId_city_temp,DomId_city_desciption,DomId_city_cloud,DomId_city_wind,
-							DomId_city_pressure,DomId_city_humidity,DomId_city_sunrise,DomId_city_sunset,DomId_city_geocoords){
+function genCurrentCityHtml(xml,listIds,city_id){
 	var xmlDomId_current = xml.getElementsByTagName("current")[0];
 	var get_last_update_time = new Date(xmlDomId_current.childNodes[9].getAttributeNode("value").nodeValue);
 	var get_sunrise_time = new Date(xmlDomId_current.childNodes[0].childNodes[2].getAttributeNode("rise").nodeValue);
 	var get_sunrset_time = new Date(xmlDomId_current.childNodes[0].childNodes[2].getAttributeNode("set").nodeValue);
 	var weather_icon = "i" + xmlDomId_current.childNodes[8].getAttributeNode("icon").nodeValue;
-
-	DomId_get_time.innerHTML = get_last_update_time.toLocaleString();
-	DomId_city_name.innerHTML = xmlDomId_current.childNodes[0].getAttributeNode("name").nodeValue + ", " + xml.getElementsByTagName("city")[0].childNodes[1].childNodes[0].nodeValue;
-	DomId_city_weather_img.classList.add(weather_icon);
-	DomId_city_temp.innerHTML = xml.getElementById(city_id).nextSibling.getAttributeNode("value").nodeValue;
-	DomId_city_desciption.innerHTML = xmlDomId_current.childNodes[8].getAttributeNode("value").nodeValue;
-	DomId_city_cloud.innerHTML = xmlDomId_current.childNodes[5].getAttributeNode("name").nodeValue;
-	DomId_city_wind.innerHTML = xmlDomId_current.childNodes[4].childNodes[0].getAttributeNode("name").nodeValue + " " + 
+	$("#get_time").html(get_last_update_time.toLocaleString());
+	$("#city_name").html(xmlDomId_current.childNodes[0].getAttributeNode("name").nodeValue + ", " + xml.getElementsByTagName("city")[0].childNodes[1].childNodes[0].nodeValue);
+	$("#city_weather-img").addClass(weather_icon);
+	$("#city_temp").html(xml.getElementById(city_id).nextSibling.getAttributeNode("value").nodeValue);
+	$("#city_desciption").html(xmlDomId_current.childNodes[8].getAttributeNode("value").nodeValue);
+	$("#city_cloud").html(xmlDomId_current.childNodes[5].getAttributeNode("name").nodeValue);
+	$("#city_wind").html(xmlDomId_current.childNodes[4].childNodes[0].getAttributeNode("name").nodeValue + " " + 
 								xmlDomId_current.childNodes[4].childNodes[0].getAttributeNode("value").nodeValue + " m/s" + "<br>" +
 								xmlDomId_current.childNodes[4].childNodes[2].getAttributeNode("name").nodeValue + "(" +
-								xmlDomId_current.childNodes[4].childNodes[2].getAttributeNode("value").nodeValue + ")";
-	DomId_city_pressure.innerHTML = xmlDomId_current.childNodes[3].getAttributeNode("value").nodeValue + " hpa";
-	DomId_city_humidity.innerHTML = xmlDomId_current.childNodes[2].getAttributeNode("value").nodeValue + "%";
-	DomId_city_sunrise.innerHTML = get_sunrise_time.getHours().toLocaleString() + ":" + get_sunrise_time.getMinutes().toLocaleString();
-	DomId_city_sunset.innerHTML = get_sunrset_time.getHours().toLocaleString() + ":" + get_sunrset_time.getMinutes().toLocaleString();
-	DomId_city_geocoords.innerHTML = xmlDomId_current.childNodes[0].childNodes[0].getAttributeNode("lon").nodeValue + ", " +
-									 xmlDomId_current.childNodes[0].childNodes[0].getAttributeNode("lat").nodeValue;
+								xmlDomId_current.childNodes[4].childNodes[2].getAttributeNode("value").nodeValue + ")");
+	$("#city_pressure").html(xmlDomId_current.childNodes[3].getAttributeNode("value").nodeValue + " hpa");
+	$("#city_humidity").html(xmlDomId_current.childNodes[2].getAttributeNode("value").nodeValue + "%");
+	$("#city_sunrise").html(get_sunrise_time.getHours().toLocaleString() + ":" + get_sunrise_time.getMinutes().toLocaleString());
+	$("#city_sunset").html(get_sunrset_time.getHours().toLocaleString() + ":" + get_sunrset_time.getMinutes().toLocaleString());
+	$("#city_geocoords").html(xmlDomId_current.childNodes[0].childNodes[0].getAttributeNode("lon").nodeValue + ", " +
+									 xmlDomId_current.childNodes[0].childNodes[0].getAttributeNode("lat").nodeValue);
 }
 
 function xHR(url,method,format,callback) {
@@ -127,25 +112,12 @@ function xHR(url,method,format,callback) {
 
 // global
 var record_city;
-var DomId_nextday,DomId_get_time,DomId_city_name,DomId_city_weather_img,DomId_city_temp,DomId_city_desciption,DomId_city_cloud,DomId_city_wind,DomId_city_pressure,
-	DomId_city_humidity,DomId_city_sunrise,DomId_city_sunset,DomId_city_geocoords;
+var DomId_nextday;
 function mainFunc(){
 	var url_7days = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Taipei&mode=json&units=metric&cnt= 7&appid=44db6a862fba0b067b1930da0d769e98";
 	var url_current_city = "http://api.openweathermap.org/data/2.5/weather?q=Taipei,tw&mode=xml&units=metric&appid=44db6a862fba0b067b1930da0d769e98";
 	var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	var DomId_nextday = document.getElementById("nextDay");
-	var DomId_get_time = document.getElementById("get_time");
-	var DomId_city_name = document.getElementById("city_name");
-	var DomId_city_weather_img = document.getElementById("city_weather-img");
-	var DomId_city_temp = document.getElementById("city_temp");
-	var DomId_city_desciption = document.getElementById("city_desciption");
-	var DomId_city_cloud = document.getElementById("city_cloud");
-	var DomId_city_wind= document.getElementById("city_wind");
-	var DomId_city_pressure = document.getElementById("city_pressure");
-	var DomId_city_humidity = document.getElementById("city_humidity");
-	var DomId_city_sunrise = document.getElementById("city_sunrise");
-	var DomId_city_sunset = document.getElementById("city_sunset");
-	var DomId_city_geocoords = document.getElementById("city_geocoords");
 	var city_id = 1668341;
 
 	$("#container_currentCity").show();
@@ -161,8 +133,7 @@ function mainFunc(){
 			genNextDayHtml(result,month,j,DomId_nextday);
 		}
 		xHR(url_current_city,"GET","xml",function(xml_obj){
-			genCurrentCityHtml(xml_obj,0,city_id,DomId_get_time,DomId_city_name,DomId_city_weather_img,DomId_city_temp,DomId_city_desciption,DomId_city_cloud,DomId_city_wind,
-				DomId_city_pressure,DomId_city_humidity,DomId_city_sunrise,DomId_city_sunset,DomId_city_geocoords);
+			genCurrentCityHtml(xml_obj,0,city_id);
 		});	
 	 });
 }
