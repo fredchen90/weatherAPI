8ad447a7f010186a5f0713c1a9a2d53039e189dc
fredchen90 <fredchen90@gmail.com>
2016-03-17 15:54:41 +0800

    完成angularJS轉移
    1.全部改用angular完成

--- weatherAPI/angular_version/controller.js
+++ weatherAPI/angular_version/controller.js
@@ -4,14 +4,20 @@ var app = angular.module('myapp', []);
 app.controller('tabController',  ['$scope', function($scope) {
 	$scope.city_class = "tab-focus";
 	$scope.day_class = "tab";
+	$scope.record_name = "taipei";
+	
 	$scope.switchTab = function(tab) {
 		$scope.active_tab = tab;
 		if($scope.active_tab == 'city'){
 			$scope.city_class = "tab-focus";
 			$scope.day_class ="tab";
+			$scope.content1_class = "show";
+			$scope.content2_class = "hide";
 		}else{
 			$scope.day_class ="tab-focus";
 			$scope.city_class = "tab";
+			$scope.content2_class = "show";
+			$scope.content1_class = "hide";
 		}
 	};
 }]);
@@ -19,45 +25,46 @@ app.controller('tabController',  ['$scope', function($scope) {
 
 // first content controller
 app.controller('firstContentController',  function($scope, $http) {
-	// $http.get("http://api.openweathermap.org/data/2.5/weather?q=Taipei,tw&mode=xml&units=metric&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
-	// 	console.log(response);
-	// })
 	$scope.citys = [
 		{name: '請選擇', id: 0},
 		{name: 'Taipei', id: 'taipei,tw'},
 		{name: 'New York', id: 'newyork,us'},
 		{name: 'London', id: 'london,uk'}
-	]
-	$scope.infos = [
-		{name: 'Wind', value: 10},
-		{name: 'Cloudiness', value: 'sky'},
-		{name: 'Pressure', value: '150 hpa'},
-		{name: 'Humidity', value: '25%'},
-		{name: 'Sunrise', value: '19:05'},
-		{name: 'Sunset', value: '06"05'},
-		{name: 'Geo coords', value: '[121.53, 25.05]'}
-	]
-	$scope.onChange = function(city_name) {
-		if($scope.myCity.id == 'taipei,tw'){
-			alert("taipei");
-		}else if($scope.myCity.id == 'newyork,us'){
-			alert("newyork");
-		}else{
-			alert("london");
-	}
+	];
+
+	$http.get("http://api.openweathermap.org/data/2.5/weather?q=Taipei,tw&mode=xml&units=metric&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
+		var x2js = new X2JS();
+		var json = x2js.xml_str2json(response.data);
+		$scope.infos = json;
+	});
+
+	$scope.onChangeCity = function() {
+		$scope.record_name = $scope.myCity.name;
+		$http.get("http://api.openweathermap.org/data/2.5/weather?q="+$scope.myCity.id+"&mode=xml&units=metric&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
+			var x2js = new X2JS();
+			var json = x2js.xml_str2json(response.data);
+			$scope.infos = json;
+		});
 	};
 
 });
 
 // second content controller
 app.controller('secondContentController', function($scope, $http) {
-	$http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=taipei&mode=json&units=metric&cnt=5&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
-		$scope.weather_img = "icon";
-		$scope.jsonformat = response.data.list;
-	})
 	$scope.days = [
 		{name:'5 days', value: 5},
 		{name:'6 days', value: 6},
 		{name:'7 days', value: 7}
-	]
+	];
+
+	// default
+	$http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=taipei&mode=json&units=metric&cnt=&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
+		$scope.jsonformat = response.data.list;
+	})
+
+	$scope.onChangeDays = function() {
+		$http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q="+$scope.record_name+"&mode=json&units=metric&cnt="+$scope.mydays.value+"&appid=5df396cf7b4d7b339bc250ebfd041f2e").then(function(response){
+			$scope.jsonformat = response.data.list;
+		})
+	};
 });
